c5541913974c8f1b022aa9959d3f2241b5cc8f22
zhengjianliu <comezheng@gmail.com>
2021-02-01 21:07:58 -0500

    CORS added

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,3 +3,14 @@ exports.getPosts = (req,res,next)=>{
         posts:[{title:"first post",content:"hey"}]
     })
 }
+
+
+exports.createPost = (req,res,next) =>{
+    const title = req.body.title
+    const content = req.body.content
+    // Create post in db
+    res.status(201).json({
+        message: 'Post created successfully!',
+        post:{id: new Date().toISOString(), title: title, content: content}
+    })
+}
